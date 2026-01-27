8b52e717ec6f6cc64e86f360c4d69c8058ebf3cc
gtugaw <genesis.tugawin@georgebrown.ca>
2026-01-27 13:48:03 -0500

    add w03 solutions

---
+++ Week03/Q1.py
@@ -0,0 +1,11 @@
+# Question 1; Student Grades List (Lists - Basic Operations)
+grades = [85, 92, 78, 95, 88]
+print(grades)
+
+grades.append(90)
+grades.sort()
+print("Sorted grades:", grades)
+print("Highest grade:", grades[-1])
+print("Lowest grade:", grades[0])
+print("Total number of grades:", len(grades))
+
